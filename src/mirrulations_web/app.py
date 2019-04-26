2771a314e38a4cf9343dab5dc6dd0b2497148ceb
Manny <stmde03@moravian.edu>
2019-04-26 14:35:32 -0400

    This update is for the website. A flashing message for a failed docId entry

--- src/mirrulations_web/app.py
+++ src/mirrulations_web/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_file
-from flask import request, render_template
+from flask import request, render_template, flash
 import mirrulations_web.download_processor as download_processor
 import mirrulations_web.dir_search as dir_search
 
@@ -8,11 +8,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    error = None
     # this block is only entered when the form is submitted
     if request.method == 'POST':
         docid = request.form.get('DirId')
         path = dir_search.search_for_document_in_directory(docid)
-        if path is not '':
+        if path is '':
+            error ='Invalid DocId'
+            return render_template("index.html", error=error)
+        elif path is not '':
             return send_file(download_processor.download_zip(docid),
                              attachment_filename=docid +
                              '.zip', as_attachment=True)
